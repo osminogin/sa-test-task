1197617cec50d49c64f79ea66122fd883c608b5a
Vladimir Osintsev <osintsev@gmail.com>
2019-10-08 23:41:22 +0300

    Authorization token added

--- speechanalytics_api/tests/conftest.py
+++ speechanalytics_api/tests/conftest.py
@@ -3,6 +3,12 @@ import pytest
 from speechanalytics_api import build_app
 
 
+@pytest.fixture
+def auth():
+    encoded_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.19X7kKD-m0ImEdCIeDzitU10IQLQIhTkGA55FOaLUhs'
+    return {'Authorization': f'Bearer {encoded_token}'}
+
+
 @pytest.fixture(scope='function')
 async def async_client(aiohttp_client):
     return await aiohttp_client(build_app)
