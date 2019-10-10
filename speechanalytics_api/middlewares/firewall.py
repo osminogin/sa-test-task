45508b35bc371809af78fd48817cd4e2012e5a76
Vladimir Osintsev <osintsev@gmail.com>
2019-10-10 10:50:10 +0300

    Fixes

--- speechanalytics_api/middlewares/firewall.py
+++ speechanalytics_api/middlewares/firewall.py
@@ -1,4 +1,4 @@
-from aiohttp.web import middleware, Response
+from aiohttp.web import middleware, HTTPForbidden, Response
 
 from ..settings import WHITELIST_IP
 
@@ -6,6 +6,6 @@ from ..settings import WHITELIST_IP
 @middleware
 async def firewall_middleware(request, handler):
     if request.remote not in WHITELIST_IP:
-        return Response(status=403)     # Forbidden
+        return Response(status=HTTPForbidden.status_code)
     response = await handler(request)
     return response
