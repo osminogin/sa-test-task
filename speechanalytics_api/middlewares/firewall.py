fb81bed0768be2d315ae1bf4334860540cf2a08d
Vladimir Osintsev <osintsev@gmail.com>
2019-10-15 12:56:47 +0300

    App firewall settings updated

--- speechanalytics_api/middlewares/firewall.py
+++ speechanalytics_api/middlewares/firewall.py
@@ -1,12 +1,12 @@
 from aiohttp.web import middleware, HTTPForbidden, Response
 
-from ..settings import WHITELIST_IP
+from ..settings import WHITELIST_IPS
 
 
 @middleware
 async def firewall_middleware(request, handler):
     """ Restrict access to whitelist middleware. """
-    if request.remote not in WHITELIST_IP:
+    if request.remote not in WHITELIST_IPS:
         return Response(status=HTTPForbidden.status_code)
     response = await handler(request)
     return response
--- speechanalytics_api/settings.py
+++ speechanalytics_api/settings.py
@@ -4,14 +4,14 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'aeghu0an0Ben9zai5Etae1eid2xoh,Ge')
 YADISK_TOKEN = os.getenv('YADISK_TOKEN')    # Required
 YADISK_CALLDATA = os.getenv('YADISK_CALLDATA',
                             '/speechanalytics-connect/meta/calls-info.csv')
-WHITELIST_IP = (
+WHITELIST_IPS = os.getenv('WHITELIST_IPS', (
     '127.0.0.1',
     '188.227.75.70',
     '188.227.75.56',
     '5.200.35.203',
     '78.140.220.147',
     '95.213.131.210',
-)
+)).split(',')
 
 LOGGING = {
     'version': 1,
